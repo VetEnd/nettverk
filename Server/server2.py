a604e5c1421ac439d9e54cf59326cc45b4ea6aaa
vetle <endrerudvetle@gmail.com>
2022-03-16 22:34:56 +0100

    Update server2.py

--- Server/server2.py
+++ Server/server2.py
@@ -10,25 +10,27 @@ ADDRESS = "127.0.0.1"
 broadcast_list = []
 my_socket.bind((ADDRESS, PORT))
 
-class newclients(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
 
-    def run(self):
-        i = 0
-        while i < 10:
-            my_socket.listen()
-            conn, addr = my_socket.accept()
-            broadcast_list.append(conn)
-            print(f"kjører")
-            message = conn.recv(1024).decode()
-            if message:
-                print(f"Received message: " + message)
-                broadcast(message)
-            else:
-                print(f" disconnected : " + conn)
-                return  
-  
+
+def listen():
+    i = 0
+    while i < 10:
+        my_socket.listen()
+        conn, addr = my_socket.accept()
+        broadcast_list.append(conn)
+        print(f"kjører")
+
+def msg(conn):
+    i=0
+    while i < 9:
+        message = conn.recv(1024).decode()
+        if message:
+            print(f"Received message: " + message)
+            broadcast(message)
+        else:
+            print(f" disconnected : " + conn)
+        return  
+
 def broadcast(message):
     for client in broadcast_list:
         try:
@@ -40,5 +42,5 @@ def broadcast(message):
 
 
 
-loop = newclients()
-loop.start()
+ok = threading.Thread(listen)
+ok.start()
