9dd6ed614b24a56a53a204a0c75a3e3998df46c9
Vetle Endrerud <endrerudvetle@gmail.com>
2022-03-17 15:26:10 +0100

    Update server2.py

--- Server/server2.py
+++ Server/server2.py
@@ -1,6 +1,7 @@
 #server.py
-from email import message
-import re
+
+
+
 import socket
 import threading
 
@@ -8,28 +9,29 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 PORT = 5000
 ADDRESS = "127.0.0.1"
 broadcast_list = []
-my_socket.bind((ADDRESS, PORT))
-
-
+my_socket.bind((ADDRESS, PORT))       
 
 def listen():
-    i = 0
-    while i < 10:
+    i=1
+    while i <= 10:
+        print ("listen kjører")
         my_socket.listen()
         conn, addr = my_socket.accept()
         broadcast_list.append(conn)
         print(f"kjører")
+        
+        
 
-def msg(conn):
-    i=0
-    while i < 9:
+    i = 1
+    while i <= 10:
+        print ("msg kjører")
+        conn = listen()
         message = conn.recv(1024).decode()
         if message:
             print(f"Received message: " + message)
             broadcast(message)
-        else:
-            print(f" disconnected : " + conn)
-        return  
+        
+         
 
 def broadcast(message):
     for client in broadcast_list:
@@ -40,7 +42,6 @@ def broadcast(message):
             print(f"Client removed : {client}")
 
 
+listen_thread = threading.Thread(target=listen)
 
-
-ok = threading.Thread(listen)
-ok.start()
+listen_thread.start()
